bd0e7f46abbe311a41a8a4d24d1985037d7b2417
xgh-dev <160808601+xgh-dev@users.noreply.github.com>
2025-01-04 17:49:40 -0600

    css de HomeScreen

--- Creamy-Buns/src/App.jsx
+++ Creamy-Buns/src/App.jsx
@@ -1,24 +1,24 @@
-import './App.css'
-import Pedidos from './components/Pedidos'
-import Recetario from './components/Recetario'
-import {Routes,Route,BrowserRouter} from 'react-router-dom'
-import HomeScreen from './components/HomeScreen'
+import "./App.css";
+import Pedidos from "./components/Pedidos";
+import Recetario from "./components/Recetario";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import HomeScreen from "./components/HomeScreen";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
   return (
     <>
-      <div>
-        <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomeScreen/>}/>
-          <Route path='/recetario' element={<Recetario/>}/>
-          <Route path='/pedidos' element={<Pedidos/>}/>
-        </Routes>
-        </BrowserRouter>
-      </div>
+        <div className="HomeScreenView">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/recetario" element={<Recetario />} />
+              <Route path="/pedidos" element={<Pedidos />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Creamy-Buns/src/components/HomeScreen.jsx
+++ Creamy-Buns/src/components/HomeScreen.jsx
@@ -3,17 +3,15 @@ import { Link } from "react-router-dom";
 const HomeScreen = () => {
   return (
     <>
-      <h1>Home Screen</h1>
-      <div>
-        <Link to={'/recetario'} >
-          <div>
-              <h2>Recetario</h2>
-          </div>
+      <h1 className="homeScreenTitle">Creamy Buns</h1>
+      <div className="container">
+        <Link to={"/recetario"} className="card">
+          <img src="/path-to-recetario-image.jpg" alt="Recetario" />
+          <h2>Recetario</h2>
         </Link>
-        <Link to={'/pedidos'}>
-        <div>
-            <h2>Pedidos</h2>
-        </div>
+        <Link to={"/pedidos"} className="card">
+          <img src="/path-to-pedidos-image.jpg" alt="Pedidos" />
+          <h2>Pedidos</h2>
         </Link>
       </div>
     </>
