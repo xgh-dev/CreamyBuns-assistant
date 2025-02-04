029f589a9a82214d128d9ff77fdd2d67be14a7e1
xgh-dev <160808601+xgh-dev@users.noreply.github.com>
2025-02-03 23:57:40 -0600

    ajustes en la concatenacion de /r para ver datos

--- Creamy-Buns/src/components/infoRecetas/InfoDeReceta.jsx
+++ Creamy-Buns/src/components/infoRecetas/InfoDeReceta.jsx
@@ -6,24 +6,68 @@ const InfoDeReceta = () => {
   //desestruturaremos los hooks de la consulta para poder apoyarnos de estos
   const { isLoading, error, datos } = useObtenerRecetaPorID(id); //mandar un condicional
   // para no tener multiples ejecuciones
-  let ingredientes = [] //lista que mandaremos al html
+  let ingredientes = []; //lista que mandaremos al html
+  let procedimiento = [];
+  let observaciones = [];
+
   if (isLoading == false) {
-    let ingrediente = '' //esta variable crea el string de un ingrediente y la manda a la variable de la lista
-    let llave = 0 //para poder incrementar este contador lo declaramos afuera del for y dentro del for lo incrementamos, para que con cada iteracion este se incremente 
+    //de esta forma accedemos al ultimo caracter del array datos.ingredientes[datos.ingredientes.length - 1] 
+    if (datos.ingredientes[datos.ingredientes.length - 1] != '\r'){
+      datos.ingredientes = datos.ingredientes.concat('\r');
+      //concat es un metodo de lista por lo tanto nos retorna una lista nueva y por lo tanto esta lista debemos guardarla en algun lugar, este lugar siendo el mismo lugar de donde surge la lista
+    }
+    if (datos.procedimiento[datos.procedimiento.length - 1] != '\r'){
+      datos.procedimiento = datos.procedimiento.concat('\r');
+    }
+    if (datos.observaciones[datos.observaciones.length - 1] != '\r'){
+      datos.observaciones = datos.observaciones.concat('\r');
+    }
+
+    console.log(datos);
+    let ingrediente = ""; //esta variable crea el string de un ingrediente y la manda a la variable de la lista
+    let llave = 0; //para poder incrementar este contador lo declaramos afuera del for y dentro del for lo incrementamos, para que con cada iteracion este se incremente
     for (let index = 0; index < datos.ingredientes.length; index++) {
-      if (datos.ingredientes[index] !== '\r'){
+      if (datos.ingredientes[index] !== "\r") {
         //console.log(datos.ingredientes[index])//hasta aqui nos imprime cada elemento del string
-        ingrediente = ingrediente + datos.ingredientes[index]
+        ingrediente = ingrediente + datos.ingredientes[index];
       } else {
         //console.log('ejecutando else')
-        llave = llave + 1
-        ingredientes.push(<li key={llave} className="ingrediente-item">{ingrediente}</li>)
-        ingrediente = ''
+        llave = llave + 1;
+        ingredientes.push(
+          <li key={llave} className="ingrediente-item">
+            {ingrediente}
+          </li>
+        );
+        ingrediente = "";
+      }
+    }
+
+    let paso = "";
+    let numeroDePaso = 0;
+    for (let index = 0; index < datos.procedimiento.length; index++) {
+      if (datos.procedimiento[index] !== "\r") {
+        //creamos la concatenacion de cada elemento del array
+        paso = paso + datos.procedimiento[index];
+      } else {
+        //creamos el push y receteamos la variable que creara el paso
+        numeroDePaso = numeroDePaso + 1;
+        procedimiento.push(<li key={procedimiento}>{paso}</li>);
+        paso = "";
+      }
+    }
+    let observacion = "";
+    let numeroDeObservacion = 0;
+    for (let index = 0; index < datos.observaciones.length; index++) {
+      if (datos.observaciones[index] !== "\r") {
+        observacion = observacion + datos.observaciones[index];
+      } else {
+        numeroDeObservacion = numeroDeObservacion + 1;
+        observaciones.push(<li key={numeroDeObservacion}>{observacion}</li>);
+        observacion = "";
       }
     }
   }
   //console.log(ingredientes)
-
 
   return (
     <>
@@ -49,26 +93,16 @@ const InfoDeReceta = () => {
                 <h3 className="section-title">Ingredientes</h3>
                 <article className="ingredientes-article">
                   <h4 className="sub-title">Base</h4>
-                  <ul className="ingredientes-list">
-                    {ingredientes}
-                  </ul>
+                  <ul className="ingredientes-list">{ingredientes}</ul>
                 </article>
               </section>
               <section className="procedimiento-section">
                 <h3 className="section-title">Procedimiento</h3>
-                <ol className="procedimiento-list">
-                  <li className="procedimiento-item">paso</li>
-                  <li className="procedimiento-item">paso</li>
-                  <li className="procedimiento-item">paso</li>
-                  <li className="procedimiento-item">paso</li>
-                  <li className="procedimiento-item">paso</li>
-                </ol>
+                <ol className="procedimiento-list">{procedimiento}</ol>
               </section>
               <section className="observaciones-section">
                 <h3 className="section-title">Observaciones</h3>
-                <p className="observaciones-text">
-                  - consejo - consejo - consejo
-                </p>
+                <ul className="observaciones-text">{observaciones}</ul>
               </section>
             </main>
           </div>
