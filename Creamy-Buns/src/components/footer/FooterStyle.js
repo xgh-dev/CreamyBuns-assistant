d304e63cec405c5f7f2a5f397114fe6394a6eff0
xgh-dev <160808601+xgh-dev@users.noreply.github.com>
2025-02-24 18:22:41 -0600

    css

--- Creamy-Buns/src/components/footer/FooterStyle.js
+++ Creamy-Buns/src/components/footer/FooterStyle.js
@@ -11,20 +11,25 @@ export const FooterList = styled.ul`
     justify-content: space-evenly;
     padding: 0;
     margin: 0;
+    padding: 10px 0;
 `;
 
 export const IconStyledFaHome = styled(FaHome)`
     font-size: 40px;
+    color: #000;
 `
 export const IconStyledFaBook = styled(FaBook)`
     font-size: 40px;
+    color: #000;
 `
 
 export const IconStyledHiAnnotation = styled(HiAnnotation)`
     font-size: 40px;
+    color: #000;
 `
 
 export const IconStyledFaUsers = styled(FaUsers)`
     font-size: 40px;
+    color: #000;
 `
 
