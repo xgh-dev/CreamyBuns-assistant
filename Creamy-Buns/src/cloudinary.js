cb93917d736d6d510bb3b09b9fa04ebcb1a19ba2
xgh-dev <160808601+xgh-dev@users.noreply.github.com>
2025-03-22 18:42:56 -0600

    funcion de cloudinary agregada al frontend

---
+++ Creamy-Buns/src/cloudinary.js
@@ -0,0 +1,28 @@
+const ApiKey = import.meta.env.VITE_API_KEY;
+const CloudName = import.meta.env.VITE_CLOUD_NAME;
+
+export const loadImage = async (file) => {
+  //crear un formDaata para generar la consulta
+  const formData = new FormData();
+  //agregamos parametros al form data
+  formData.append("file", file); //agrega el archivo
+  //indica la carpeta
+  formData.append("folder", "CreamyBuns");
+  //indicar la api_key
+  formData.append("api_key", ApiKey);
+  //indicar el contador de feha
+  formData.append("timestamp", (Date.now() / 1000) | 0);
+  //indicar el signature, no es neesario
+
+  //ahora debemos hacer el fetch que consule a la api y mande el form data
+  const consultaCloaudinary = await fetch(
+    `https://api.cloudinary.com/v1_1/${CloudName}/image/upload
+`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
+  const respuesta = await consultaCloaudinary.json()
+  console.log(respuesta)
+};
